fa161703bea2e9371520e10f0f41f302459e7f92
mihiryadav20 <mihir.yadav@somaiya.edu>
2025-11-19 15:02:06 +0530

    lint fixes

--- react/src/App.tsx
+++ react/src/App.tsx
@@ -1,9 +1,14 @@
 import { Button } from "@/components/ui/button"
+import { useTheme } from "./components/ui/theme-provider"
 
 function App() {
+  const { theme, setTheme } = useTheme()
+
   return (
     <div className="flex min-h-svh flex-col items-center justify-center bg-background text-foreground">
-      <Button>Click me</Button>
+      <Button onClick={() => setTheme(theme === "dark" ? "light" : "dark")}>
+        {theme === "dark" ? "Switch to Light Mode" : "Switch to Dark Mode"}
+      </Button>
     </div>
   )
 }
